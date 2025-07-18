0952e1908bade66ab054889cd2116393cde226eb
Arthur <thurarquivos@gmail.com>
2025-07-17 21:14:04 -0300

    ultimo

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     namespace = "com.example.flappy_bird"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
